47050d0df46110c3261eb98edde71d9987ef6685
Scott O'Brien <scott@scottyob.com>
2025-01-03 11:17:44 -0800

    Added clang-formatting

--- src/vario/PageMenuVario.h
+++ src/vario/PageMenuVario.h
@@ -2,32 +2,25 @@
 #define page_menu_units_h
 
 #include <Arduino.h>
-#include "menu_page.h"
+
 #include "buttons.h"
+#include "menu_page.h"
 
 class VarioMenuPage : public SettingsMenuPage {
-  public:
-    VarioMenuPage() {
-      cursor_position = 0;
-      cursor_max = 7;
-    }
-    void draw();
+ public:
+  VarioMenuPage() {
+    cursor_position = 0;
+    cursor_max = 7;
+  }
+  void draw();
 
-  protected:
-    void setting_change(Button dir, ButtonState state, uint8_t count);
+ protected:
+  void setting_change(Button dir, ButtonState state, uint8_t count);
 
-  private:
-    static constexpr char * labels[8] = {
-      "Back",
-      "BeepVolume",
-      "Sens",
-      "Tones",
-      "LiftyAir",
-      "ClimbAvg",
-      "ClimbStart",
-      "SinkAlarm",  
-    };
+ private:
+  static constexpr char* labels[8] = {
+      "Back", "BeepVolume", "Sens", "Tones", "LiftyAir", "ClimbAvg", "ClimbStart", "SinkAlarm",
+  };
 };
-
 
 #endif
