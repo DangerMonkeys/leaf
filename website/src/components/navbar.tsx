950d85da917feca77c54f09d834c4190c7a59767
Scott O'Brien <scott@scottyob.com>
2025-02-06 22:24:56 -0800

    Navbar works with build
    - Still a small bug where the underline doesn't always show the right mode though.

--- website/src/components/navbar.tsx
+++ website/src/components/navbar.tsx
@@ -4,28 +4,13 @@ import styled from 'styled-components'
 
 const HEIGHT = "h-[70px]"
 
+
 function NavBarItem(props: { href: string, name: string, selected: boolean }) {
     const { href, name, selected } = props;
+    const item = <div className={`pt-2 pb-3 ${selected ? "text-gray-900" : "text-gray-600"} hover:text-gray-900 font-semibold`}>{name}</div>;
 
-    const UnderlinedA = styled.a`
-        ${selected && `&:after {
-            content: "";
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            background-color: #ffb459;
-            height: 3px;
-            width: calc(100%);
-        }`}
-    `
-
-    return <li className={`
-    `}
-    >
-        <UnderlinedA href={href} className="relative">
-            <div className={`pt-2 pb-3 ${selected ? "text-gray-900" : "text-gray-600"} hover:text-gray-900 font-semibold`}>{name}</div>
-        </UnderlinedA>
-        {/* <div className="bg-orange-600 h-[3px] absolute bottom-0">.</div> */}
+    return <li>
+        <a href={href} className={`relative ${selected && "highlighted"}`}>{item}</a>
     </li>
 }
 
@@ -49,7 +34,7 @@ export default function NavBar() {
                     }
                 })
             },
-            { threshold: 0.90 } // Trigger more frequently
+            { threshold: 0.50 } // Trigger more frequently
         );
 
         Object.keys(sections).forEach((id) => {
@@ -64,20 +49,7 @@ export default function NavBar() {
     return (
         <>
             {/* Navbar */}
-            <div className={`
-                z-50
-                sm:flex
-                fixed 
-                w-full 
-                text-green-950
-                bg-white
-                border-b-gray-300
-                border-b
-                pl-8
-                hidden
-
-                `}
-            >
+            <div className="z-50 sm:flex fixed w-full text-green-950 bg-white border-b-gray-300 border-b pl-8 hidden">
                 <ul className="flex space-x-24 items-end grow justify-center">
                     {Object.entries(sections).map(([id, name]) => (
                         <NavBarItem key={id} href={`#${id}`} name={name} selected={id == lastHash} />
