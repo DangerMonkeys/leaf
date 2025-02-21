609b43037852e14e036a55350385d4f6c8144ba4
Scott O'Brien <scott@scottyob.com>
2025-02-21 08:11:46 -0800

    Variant
    * Added support for hardware variant configuration
    * Added variants for the Leaf v3.2.2 and v3.2.3 hardware.

---
+++ src/variants/leaf_3_2_2/variant.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// v3.2.2 uses an older display WO256X128
+#define WO256X128
+#define DISPLAY_180_ROTATE
+
+// For OTA updates
+#define LEAF_FIRMWARE_NAME "leaf_3_2_2"
---
+++ src/vario/configuration.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// First try to load up any config from variants
+#ifdef VARIANT
+#include "variant.h"
+#endif
