c51770f107cc6e6b1a959c69b959682d8feb7cd7
Benjamin Pelletier <pelletier@alumni.caltech.edu>
2025-03-11 18:44:21 -0700

    Update from latest_versions.json

--- src/vario/version.h
+++ src/vario/version.h
@@ -4,7 +4,7 @@
 // at build time via src/scripts/versioning.py:
 // ---------------------------------------------------------------
 // Full semantic version (e.g., "0.9.0-9bbad.dev+h2.3.5")
-#define FIRMWARE_VERSION "0.0.9-a81+h3.2.5"
+#define FIRMWARE_VERSION "0.0.9-628d+h3.2.5"
 // Latest tag version (e.g., "0.9.0")
 #define TAG_VERSION "0.0.9"
 // Hardware variant name (e.g., "leaf_3_2_5")
@@ -15,6 +15,4 @@
 #define OTA_HOST "github.com"
 #define OTA_BIN_FILENAME \
   "/DangerMonkeys/leaf/releases/latest/download/firmware-" HARDWARE_VARIANT ".bin"
-// TODO: Get latest tag version for current hardware variant from latest_versions.json instead of
-// leaf.version
-#define OTA_VERSION_FILENAME "/DangerMonkeys/leaf/releases/latest/download/leaf.version"
+#define OTA_VERSIONS_FILENAME "/DangerMonkeys/leaf/releases/latest/download/latest_versions.json"
