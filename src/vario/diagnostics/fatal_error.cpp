9463e2f8319f92db9802388954cdfd0e1a1f1ac0
Benjamin Pelletier <pelletier@alumni.caltech.edu>
2025-07-05 11:41:58 -0700

    Enhance fatal error behavior

--- src/vario/diagnostics/fatal_error.cpp
+++ src/vario/diagnostics/fatal_error.cpp
@@ -6,7 +6,15 @@
 
 #include "FS.h"
 
+#include "ui/audio/speaker.h"
+#include "ui/display/display.h"
+#include "ui/display/fonts.h"
+#include "ui/input/buttons.h"
+#include "ui/settings/settings.h"
+
 File fatal_error_file;
+
+constexpr size_t BUFFER_SIZE = 512;
 
 bool useFile() {
   if (fatal_error_file) {
@@ -38,12 +46,11 @@ bool useFile() {
 }
 
 void fatalErrorInfo(const char* msg, ...) {
-  constexpr size_t bufferSize = 512;
-  char buffer[bufferSize];
+  char buffer[BUFFER_SIZE];
 
   va_list args;
   va_start(args, msg);
-  vsnprintf(buffer, bufferSize, msg, args);
+  vsnprintf(buffer, BUFFER_SIZE, msg, args);
   va_end(args);
 
   Serial.println(buffer);
@@ -52,28 +59,87 @@ void fatalErrorInfo(const char* msg, ...) {
   }
 }
 
+void displayFatalError(char* msg) {
+  u8g2.firstPage();
+  do {
+    // TODO: Add skull and cross bones icon
+
+    u8g2.setFont(leaf_6x12);
+    u8g2.setCursor(0, 12);
+    u8g2.print("FATAL ERROR");
+
+    u8g2.setFont(leaf_5x8);
+    u8g2.setCursor(0, 36);
+    u8g2.print(msg);  // TODO: wrap words to multiple lines
+
+    u8g2.setFont(leaf_5x8);
+    u8g2.setCursor(0, 163);
+    u8g2.print("Please report this");
+    u8g2.setCursor(0, 172);
+    u8g2.print("error.");
+    u8g2.setCursor(0, 186);
+    u8g2.print("Hold button to reboot");
+  } while (u8g2.nextPage());
+
+  // TODO: Add QR code linking to error reporting instructions
+}
+
+void rebootOnKeyPress() {
+  Button which_button;
+  ButtonState button_state;
+
+  // Wait until no buttons are pressed
+  do {
+    which_button = buttons_check();
+  } while (which_button == Button::NONE);
+
+  // Wait until a button is held
+  do {
+    which_button = buttons_check();
+    button_state = buttons_get_state();
+  } while (button_state != ButtonState::HELD);
+
+  speaker_playSound(fx_off);
+  while (onSpeakerTimer()) {
+    delay(10);
+  }
+
+  ESP.restart();
+}
+
 void fatalError(const char* msg, ...) {
   Serial.print("FATAL ERROR: ");
 
-  constexpr size_t bufferSize = 512;
-  char buffer[bufferSize];
-
+  // Construct final error message
+  char buffer[BUFFER_SIZE];
   va_list args;
   va_start(args, msg);
-  vsnprintf(buffer, bufferSize, msg, args);
+  vsnprintf(buffer, BUFFER_SIZE, msg, args);
   va_end(args);
 
   Serial.println(buffer);
+
+  // Try to write final error message to file
   if (useFile()) {
     fatal_error_file.println(buffer);
   }
 
+  // Close file if open
   if (fatal_error_file) {
     fatal_error_file.close();
   }
 
-  // TODO: when possible, show death icon on LCD along with message
-  // TODO: when possible, play death sound
+  // Show fatal error info on screen
+  u8g2.clear();
+  displayFatalError(buffer);
 
-  while (true);
+  // Play fatal error sound
+  speaker_unMute();
+  settings.system_volume = 3;
+  speaker_playSound(fx_fatalerror);
+  while (onSpeakerTimer()) {
+    delay(10);
+  }
+
+  rebootOnKeyPress();
 }
