e37b9e14c3ed7412ba58bc0916f215848c09f9c4
James Burgess <oxothnk@gmail.com>
2025-09-17 21:18:47 -0700

    Add FCC ID's to About Page

--- src/vario/ui/display/pages/dialogs/page_menu_about.cpp
+++ src/vario/ui/display/pages/dialogs/page_menu_about.cpp
@@ -9,17 +9,19 @@
 #include "ui/display/fonts.h"
 
 void PageMenuAbout::draw_extra() {
-  auto y = 40;
+  auto y = 24;
   constexpr auto offset = 10;
 
+  // Leaf Version
   u8g2.setFont(leaf_6x12);
   u8g2.setCursor(0, y += offset);
-  u8g2.print("Ver: ");
+  u8g2.print("Leaf Version:");
   u8g2.setCursor(5, y += offset);
   u8g2.setFont(leaf_5x8);
   u8g2.print(LeafVersionInfo::firmwareVersion());
   y += offset;
 
+  // IP Address
   u8g2.setFont(leaf_6x12);
   u8g2.setCursor(0, y += offset);
   u8g2.print("Ip: ");
@@ -28,6 +30,7 @@ void PageMenuAbout::draw_extra() {
   u8g2.print(WiFi.localIP().toString());
   y += offset;
 
+  // MAC Address
   uint8_t mac[6];
   esp_efuse_mac_get_default(mac);
   String macStr = "";
@@ -39,7 +42,6 @@ void PageMenuAbout::draw_extra() {
       macStr += ":";
     }
   }
-
   u8g2.setFont(leaf_6x12);
   u8g2.setCursor(0, y += offset);
   u8g2.print("Mac: ");
@@ -48,17 +50,28 @@ void PageMenuAbout::draw_extra() {
   u8g2.print(macStr);
   y += offset;
 
+  // FCC ID
   u8g2.setFont(leaf_6x12);
   u8g2.setCursor(0, y += offset);
-  u8g2.print("Fanet Address: ");
+  u8g2.print("FCC ID:");
   u8g2.setCursor(5, y += offset);
   u8g2.setFont(leaf_5x8);
-#ifndef FANET_CAPABLE
-  u8g2.print("N/A");
-#else
-  u8g2.print(FanetRadio::getAddress());
+  u8g2.print("2AC7Z-ESPS3MINI1");  // ESP32-S3-MINI-1 module FCC ID
+#ifdef FANET_CAPABLE
+  if (fanetRadio.getState() != FanetRadioState::UNINSTALLED) {
+    u8g2.setCursor(5, y += offset);
+    u8g2.print("Z4T-WIO-SX1262");
+    y += offset;
+
+    // FANET Address
+    u8g2.setFont(leaf_6x12);
+    u8g2.setCursor(0, y += offset);
+    u8g2.print("FANET Address:");
+    u8g2.setCursor(5, y += offset);
+    u8g2.setFont(leaf_5x8);
+    u8g2.print(FanetRadio::getAddress());
+  }
 #endif
-  y += offset;
 }
 
 void PageMenuAbout::show() { push_page(this); }
