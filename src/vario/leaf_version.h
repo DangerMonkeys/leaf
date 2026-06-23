170f1ae99d52d851333e0599f55236304613ff25
oxothnk423 <oxothnk@gmail.com>
2026-06-23 09:09:06 -0700

    restore leaf_version.h

---
+++ src/vario/leaf_version.h
@@ -0,0 +1,27 @@
+#pragma once
+
+// To optimize iterative build speed, do not include this file directly; instead, use accessor
+// functions in system/version_info.h.
+
+// The values here do not matter; they are set automatically
+// at build time via src/scripts/versioning.py:
+// ---------------------------------------------------------------
+// Full semantic version (e.g., "0.9.0-9bbad.dev+h2.3.5")
+#define FIRMWARE_VERSION "0.0.0+build.script.error"
+// Latest tag version (e.g., "0.9.0")
+#define TAG_VERSION "0.0.0"
+// Hardware variant name (e.g., "leaf_3_2_5")
+#define HARDWARE_VARIANT "unknown"
+// ---------------------------------------------------------------
+
+// Where to check for the latest version
+#define OTA_ORG "DangerMonkeys"
+
+#define OTA_BIN_PATH OTA_ORG "/leaf/releases/download/v%s/firmware-" HARDWARE_VARIANT ".bin"
+#define OTA_BIN_URL "https://github.com/" OTA_BIN_PATH
+
+#define OTA_VERSIONS_PATH OTA_ORG "/leaf/releases/latest/download/latest_versions.json"
+#define OTA_VERSIONS_URL "https://github.com/" OTA_VERSIONS_PATH
+
+// If true, always perform OTA update regardless of whether already on latest version
+#define OTA_ALWAYS_UPDATE false
