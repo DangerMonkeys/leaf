5085cbe06766320bf563f1de0abe415d56a404e6
James Ryan <164094565+oxothnk423@users.noreply.github.com>
2026-07-27 09:58:15 -0700

    Tune waypoint menu UX (#366)

--- src/vario/ui/display/pages/fanet/page_fanet_stats.h
+++ src/vario/ui/display/pages/fanet/page_fanet_stats.h
@@ -6,7 +6,7 @@
 class PageFanetStats : public SimpleSettingsMenuPage {
  public:
   static void show();
-  const char* get_title() const override { return "Fanet Stats"; }
+  const char* get_title() const override { return "Stats"; }
   uint8_t get_title_glyph() const override { return menu_ui::GLYPH_FANET; }
   void draw_extra() override;
 
