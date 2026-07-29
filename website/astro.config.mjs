7e118e3757ec48e5627c6640d0454b180125c40c
oxothnk423 <oxothnk@gmail.com>
2026-07-29 14:38:07 -0700

    Add website manual compatibility pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -63,6 +63,7 @@ function getMarkdownItems(dir) {
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://leafvario.com',
     prefetch: false,
     integrations: [starlight({
         title: 'Leaf',
@@ -115,6 +116,9 @@ export default defineConfig({
                 "satellite-alt",
                 "sd-card",
                 "smartphone",
+                "home-outline",
+                "menu-book-outline",
+                "rate-review-outline",
             ]
         },
     })],
